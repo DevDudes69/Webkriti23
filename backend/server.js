e3b9065fce440e63a231fd1134a6585761310b9f
parasmahla1 <parasmahla80@gmail.com>
2023-08-22 19:36:23 +0530

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/dbConnection');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
 connectDB();
@@ -10,7 +11,7 @@ const port = process.env.PORT || 5001;
 
 app.use(express.json());
 
-
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log(`Server is listening on port ${port}`);
 })
