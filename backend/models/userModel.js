c33be3a6215d15413cb20a7aff89052144ad0671
parasmahla1 <parasmahla80@gmail.com>
2023-08-31 21:10:13 +0530

    Booking Model and routes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,22 +1,32 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema(
-//     {
-//     username: {
-//         type: String,
-//         required : [true, "Please add user name"]
-//     },
-//     email : {
-//         type: String,
-//         required: [true,"Please add user email"]
-//     },
-//     password : {
-//         type: String,
-//         required: [true,"Please enter password"]
-//     }
-// },{
-//     timestamps: true
-// }
+const userSchema = new mongoose.Schema(
+    {
+    username: {
+        type: String,
+        required : [true, "Please add user name"]
+    },
+    email : {
+        type: String,
+        required: [true,"Please add user email"]
+    },
+    password : {
+        type: String,
+        required: [true,"Please enter password"]
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    user_img: {
+        type: String
+    },
+    liked: {
+        type: String
+    }, 
+},{
+    timestamps: true
+}
 )
 
-module.exports = mongoose.model("User",userSchema);
+module.exports = mongoose.model("H_user",userSchema);
--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,19 @@ const express = require('express');
 const connectDB = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
-
+const cookieParser = require("cookie-parser")
 connectDB();
 
 const app = express();
-
-const port = process.env.PORT || 5001;
+app.use(cookieParser);
+const port = process.env.PORT || 5050;
 
 app.use(express.json());
-
+app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/hotels",require("./routes/hotelRoutes"))
+app.use("/api/booking",require('./routes/bookingsRoutes'))
 app.use(errorHandler)
+
 app.listen(port,()=>{
     console.log(`Server is listening on port ${port}`);
 })
